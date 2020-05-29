d8f5dad4ee60604b0ac09dd1fbbeba07538116d7
Valerio Marsocci <49810041+VMarsocci@users.noreply.github.com>
2020-05-29 09:00:50 +0200

    Add files via upload

--- utils/dataset.py
+++ utils/dataset.py
@@ -6,6 +6,8 @@ import torch
 from torch.utils.data import Dataset
 import logging
 from PIL import Image
+import tifffile as tiff
+import cv2
 
 class BasicDataset(Dataset):
     def __init__(self, imgs_dir, masks_dir, scale=1):
@@ -23,12 +25,14 @@ class BasicDataset(Dataset):
 
     @classmethod
     def preprocess(cls, pil_img, scale):
-        w, h = pil_img.size
+        # w, h = pil_img.size
+        w, h = pil_img.shape[:2]
         newW, newH = int(scale * w), int(scale * h)
         assert newW > 0 and newH > 0, 'Scale is too small'
-        pil_img = pil_img.resize((newW, newH))
-
-        img_nd = np.array(pil_img)
+        # pil_img = resize(pil_img, (newW, newH))
+        # pil_img = pil_img.resize((newW, newH))
+        img_nd = cv2.resize(pil_img, dsize=(newW, newH))
+        # img_nd = np.array(pil_img)
 
         if len(img_nd.shape) == 2:
             img_nd = np.expand_dims(img_nd, axis=2)
@@ -42,18 +46,23 @@ class BasicDataset(Dataset):
 
     def __getitem__(self, i):
         idx = self.ids[i]
-        mask_file = glob(self.masks_dir + idx[:-4] + '_CLS.*')
+        # mask_file = glob(self.masks_dir + idx[:-5] + '_CLS.*')
+        mask_file = glob(self.masks_dir + idx.rsplit('_',1)[0] + '_CLS.*')
         img_file = glob(self.imgs_dir + idx + '.*')
-
+        
         assert len(mask_file) == 1, \
             f'Either no mask or multiple masks found for the ID {idx}: {mask_file}'
         assert len(img_file) == 1, \
             f'Either no image or multiple images found for the ID {idx}: {img_file}'
-        mask = Image.open(mask_file[0])
-        img = Image.open(img_file[0])
+        # mask = Image.open(mask_file[0])
+        # img = Image.open(img_file[0])
+        mask = tiff.imread(mask_file)
+        img = tiff.imread(img_file)
 
-        assert img.size == mask.size, \
-            f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
+        assert img.shape[:2] == mask.shape[:2], \
+          f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
+        # assert img.size == mask.size, \
+        #   f'Image and mask {idx} should be the same size, but are {img.size} and {mask.size}'
 
         img = self.preprocess(img, self.scale)
         mask = self.preprocess(mask, self.scale)
